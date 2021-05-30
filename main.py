647b5d928288510d085695fcd6e936383d249e7c
Dario Espinola <despinola91@gmail.com>
2021-05-30 20:24:45 -0300

    Implementing logout

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import unittest
 from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 from flask_bs4 import Bootstrap
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 from app import create_app
 from app.forms import LoginForm
@@ -44,7 +44,7 @@ def index():
 @login_required
 def hello():
     user_ip = session.get('user_ip')
-    username = session.get('username')
+    username = current_user.id
 
     context = {
         'user_ip':user_ip,
@@ -52,7 +52,7 @@ def hello():
         'username': username
     }
     
-    users = get_users()
+    #users = get_users()
     # for user in users:
     #     print(user.id)
     #     print(user.to_dict()['password'])
