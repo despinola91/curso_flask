8ba2ff3cc4996bd1ee5dbb274a9eaa0256c58184
Dario Espinola <despinola91@gmail.com>
2021-05-30 01:18:31 -0300

    Working with session instead of cookies

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
 #from flask_bootstrap import Bootstrap
 from flask_bs4 import Bootstrap
+from os import urandom
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app) #Inicializamos bootstrap
+
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
+#app.config['SECRET_KEY'] = urandom(16) --> forma más segura de generar secret key
+
 
 todos = ['Comprar café', 'Enviar solicitud de compra', 'Entregar video al productor']
 
@@ -22,14 +27,14 @@ def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
 
     return response
 
 
 @app.route('/hello')
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
     context = {
         'user_ip':user_ip,
         'todos': todos
