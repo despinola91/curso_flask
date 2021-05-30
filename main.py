e248dbe8bb6ab725f404f2139e9738aa4d50a0b3
Dario Espinola <despinola91@gmail.com>
2021-05-30 12:04:23 -0300

    Adding testing module

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from flask_bs4 import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, data_required
+import unittest
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app) #Inicializamos bootstrap
@@ -20,6 +21,13 @@ class LoginForm(FlaskForm):
     username = StringField('Nombre de usuario', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Enviar')
+
+
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
+
 
 @app.errorhandler(404)
 def not_found(error):
