70c16c062f35838423663d8059dc3b4704e89f3b
Dario Espinola <despinola91@gmail.com>
2021-05-30 00:18:46 -0300

    Implementing error handlers for 404 and 500

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ app = Flask(__name__)
 
 todos = ['Comprar café', 'Enviar solicitud de compra', 'Entregar video al productor']
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('500.html', error=error)
+
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr
