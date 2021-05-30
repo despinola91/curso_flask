ceed9dcc1b48d0bcb2d890b35041e8817ba425e8
Dario Espinola <despinola91@gmail.com>
2021-05-30 03:01:29 -0300

    Adding flashes/alerts to be shown

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, make_response, redirect, render_template, session, url_for
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
+#from flask.helpers import flash
 #from flask_bootstrap import Bootstrap
 from flask_bs4 import Bootstrap
 #from os import urandom
@@ -56,6 +57,8 @@ def hello():
     if login_form.validate_on_submit():
         username = login_form.username.data
         session['username'] = username
+
+        flash('Nombre de usuario registrado con éxito!')
 
         return redirect(url_for('index'))
 
