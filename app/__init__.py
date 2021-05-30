a821b0ae8a8a6486a017cdad760bd202cee19723
Dario Espinola <despinola91@gmail.com>
2021-05-30 17:50:57 -0300

    Implementing blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_bs4 import Bootstrap
 from .config import Config
+from .auth import auth
 
 def create_app():
     app = Flask(__name__)
     bootstrap = Bootstrap(app) #Inicializamos bootstrap
 
     app.config.from_object(Config)
-
+    app.register_blueprint(auth)
     return app
